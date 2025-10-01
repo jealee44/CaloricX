057c772b95cad86a586dd79ad96ac9038999be95
Jea Hyun Lee <jealee44@gmail.com>
2025-10-01 02:22:21 -0400

    chore: env setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    dialect: "postgresql",
-    schema: "./src/db/schema,ts",
-    out: "./drizzle",
-    dbCredentials: {
-        url: process.env.DATABASE_URL as string,
-    },
-    verbose: true,
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
     strict: true,
 });
