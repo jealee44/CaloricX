601838bb311c731da4af57a9f39c0e8c3b868bf4
Jea Hyun Lee <jealee44@gmail.com>
2025-10-02 01:24:31 -0400

    added oauth with clerk and fixed svg 500 errors with clerkv6 configurations in middleware.tsx and app/page.tsx

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,21 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
-  "/favicon.ico",
   "/sign-in(.*)",
-  "/sign-up(.*)"
+  "/sign-up(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect(); 
+    await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)"]
+  matcher: [
+    // Skip Next.js internals and static files
+    '/((?!.*\\..*|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+  ],
 };
